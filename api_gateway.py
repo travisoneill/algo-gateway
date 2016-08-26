c134577f69011a9aad7288146234205e37aa85c8
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-08-26 16:56:49 -0700

    backend running

--- api_gateway.py
+++ api_gateway.py
@@ -14,6 +14,11 @@ services = {
 
 app = Flask(__name__)
 
+@app.route('/test')
+def hello():
+    print('Hello!  Api Gateway is running.')
+    return('Hello!  Api Gateway is running.')
+
 @app.route('/')
 def reroute():
     res = requests.get('http://localhost:' + ports['static']+ '/' )
@@ -43,60 +48,16 @@ def send():
     for key, request_data in incoming_data.items():
         if key[0:4] != 'data':
             continue
-        # print('______________________')
-        # print(key, request_data['name'])
-        # print('______________________')
         language = request_data['language']
         server = services[language]
         request_url = 'http://localhost:' + ports[server] + request.path
         outgoing_data = {'lengthArr': test_params, 'request_data': request_data}
         headers = {'Content-Type' : 'application/json'}
-        # print('OUTGOING')
-        # print(outgoing_data)
-        # print('JSON')
-        # print(json.dumps(outgoing_data))
         res = requests.post(request_url, data=json.dumps(outgoing_data), headers=headers)
         print(res)
         final_data[key] =  json.loads(res.content)
         print(jsonify(final_data))
     return jsonify(final_data)
-
-# @app.route('/api/algos', methods=['POST'])
-# def send():
-#     data = request.json
-#     servers = {}
-#     responses = []
-#     for service, server in services.items():
-#         servers[server] = False
-#
-#     print('servers', servers)
-#
-#     for key, params in data.items():
-#         if key[0:4] == 'data':
-#             language = params['language']
-#             server = services[language]
-#             servers[server] = True
-#
-#     print('servers', servers)
-#
-#     for server, send_status in servers.items():
-#         if send_status == True:
-#             url = 'http://localhost:' + ports[server] + request.path
-#             res = requests.post(url, data)
-#             responses.append(res)
-#             print(responses)
-#
-#     print(request.json['data1'])
-#     return "REDIRECTED"
-
-
-# def parse_dictionary(dictionary):
-#     print('DICTIONARY')
-#     print(dictionary)
-#     print(json.dumps(dictionary))
-#     return dictionary
-
-
 
 #gunicorn
 #tornado
