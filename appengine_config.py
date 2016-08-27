2c364af891134c4136b711d5e63f851c7063655c
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-08-27 16:10:36 -0700

    update config project folder

--- appengine_config.py
+++ appengine_config.py
@@ -3,4 +3,4 @@ import vendor
 # insert `lib` as a site directory so our `main` module can load
 # third-party libraries, and override built-ins with newer
 # versions.
-vendor.add('lib')
+vendor.add('venv/lib')
