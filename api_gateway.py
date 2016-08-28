34d8692fe4a0b4786ebff4614a2f50fe9ddb0bef
travis.h.oneill@gmail.com <travis.h.oneill@gmail.com>
2016-08-28 14:56:09 -0700

    remove global objects

--- api_gateway.py
+++ api_gateway.py
@@ -26,39 +26,39 @@ def reroute():
     # return res.content
     return('Hello!  Api Gateway is running.')
 
-# @app.route('/<path:params>')
-# def root(params):
-#     if params == '':
-#         res = requests.get('http://localhost:' + ports['static']+ '/' )
-#         return res.content
-#     else:
-#         url = 'http://localhost:' + ports['static']+ '/' + params
-#         return redirect(url, code=302, Response=None)
-#
-# @app.route('/api/algos', methods=['POST'])
-# def send():
-#     incoming_data = request.get_json()
-#     print("INCOMING")
-#     print(incoming_data)
-#     print("JSON")
-#     print(json.dumps(incoming_data))
-#     test_params = incoming_data['lengthArr']
-#     responses = []
-#     final_data = {}
-#     print(request.is_json)
-#     for key, request_data in incoming_data.items():
-#         if key[0:4] != 'data':
-#             continue
-#         language = request_data['language']
-#         server = services[language]
-#         request_url = 'http://localhost:' + ports[server] + request.path
-#         outgoing_data = {'lengthArr': test_params, 'request_data': request_data}
-#         headers = {'Content-Type' : 'application/json'}
-#         res = requests.post(request_url, data=json.dumps(outgoing_data), headers=headers)
-#         print(res)
-#         final_data[key] =  json.loads(res.content)
-#         print(jsonify(final_data))
-#     return jsonify(final_data)
+@app.route('/<path:params>')
+def root(params):
+    if params == '':
+        res = requests.get('http://localhost:' + ports['static']+ '/' )
+        return res.content
+    else:
+        url = 'http://localhost:' + ports['static']+ '/' + params
+        return redirect(url, code=302, Response=None)
+
+@app.route('/api/algos', methods=['POST'])
+def send():
+    incoming_data = request.get_json()
+    print("INCOMING")
+    print(incoming_data)
+    print("JSON")
+    print(json.dumps(incoming_data))
+    test_params = incoming_data['lengthArr']
+    responses = []
+    final_data = {}
+    print(request.is_json)
+    for key, request_data in incoming_data.items():
+        if key[0:4] != 'data':
+            continue
+        language = request_data['language']
+        server = services[language]
+        request_url = 'http://localhost:' + ports[server] + request.path
+        outgoing_data = {'lengthArr': test_params, 'request_data': request_data}
+        headers = {'Content-Type' : 'application/json'}
+        res = requests.post(request_url, data=json.dumps(outgoing_data), headers=headers)
+        print(res)
+        final_data[key] =  json.loads(res.content)
+        print(jsonify(final_data))
+    return jsonify(final_data)
 
 if __name__  == "__main__":
     app.run()
